782f717ab6e27bb9370c79787e79fbb344a399a7
James Reeves <jreeves@weavejester.com>
2014-05-19 23:08:04 +0100

    Prepare project file

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject listora/ring-ssl "0.1.1"
-  :description "Force SSL in your Ring application"
-  :url "https://github.com/listora/ring-ssl"
+(defproject ring/ring-ssl "0.2.0-SNAPSHOT"
+  :description "Middleware for securing handlers over HTTPS"
+  :url "https://github.com/ring-clojure/ring-ssl"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.1"]]
-  :profiles {:shared {:dependencies [[ring-mock "0.1.5"]]}
-             :dev [:shared]
-             :test [:shared]})
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.3.0-RC1"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
