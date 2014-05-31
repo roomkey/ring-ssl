00e0342d7ca9ff49f8827c50b1d07f92407a7288
James Reeves <jreeves@weavejester.com>
2014-05-31 20:29:47 +0100

    Add test profiles for Clojure 1.4, 1.5 and 1.6

--- project.clj
+++ project.clj
@@ -7,4 +7,8 @@
                  [ring/ring-core "1.3.0-RC1"]]
   :plugins [[codox "0.8.5"]]
   :codox {:project {:name "Ring-SSL"}}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
