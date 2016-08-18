6be183b031741cd967a1c0dc02e6f13b43940321
James Reeves <jreeves@weavejester.com>
2016-08-18 17:50:44 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github.com/ring-clojure/ring-ssl"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.3.0-RC1"]]
-  :plugins [[codox "0.8.5"]]
-  :codox {:project {:name "Ring-SSL"}}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.6.0-beta5"]]
+  :plugins [[lein-codox "0.9.6"]]
+  :codox {:project {:name "Ring-SSL"}
+          :output-path "codox"}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
